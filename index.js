10f256ec4344dd6620974d292669edbea9d1446e
“kiran37” <kiran.gautam@science37.com>
2019-03-14 13:39:27 -0700

    ading

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ exports.handler = async (event, context, callback) => {
   const {resource, path, httpMethod, queryStringParameters, pathParameters, body} = event;
   console.log("Incoming----\n",resource, path, httpMethod, queryStringParameters, pathParameters, body);
   const defaultMessage = {
-      'hello': 'there'
-  };
+    "statusCode": 200,
+    "body": "[\"aaa\",\"ccc\",\"ggg\",\"bbb\",\"ddd\"]"
+  }
   callback(null, defaultMessage);
 };
 
