957351bb13c9981996d30713f0af15d169461b55
“kiran37” <kiran.gautam@science37.com>
2019-03-14 13:58:58 -0700

    success message

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 'use strict';
+const CORS_KEY = 'access-control-allow-origin';
 
 exports.handler = async (event, context, callback) => {
-  const {resource, path, httpMethod, queryStringParameters, pathParameters, body} = event;
-  console.log("Incoming----\n",resource, path, httpMethod, queryStringParameters, pathParameters, body);
-  const defaultMessage = {
-    "statusCode": 200,
-    "body": "I am working fine"
-  }
-  callback(null, defaultMessage);
+  const options = {
+    method: event.httpMethod,
+    url: event.path,
+    payload: event.body,
+    headers: event.headers,
+  };
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Content-Type" : "application/json"
+    },
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+  callback(null, response);
 };
 
 
