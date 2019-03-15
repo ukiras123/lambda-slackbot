9de67ec1595cecc05c9cc6ace843ef433a4973f7
“kiran37” <kiran.gautam@science37.com>
2019-03-14 17:24:24 -0700

    final version of working copy

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
 const CORS_KEY = 'access-control-allow-origin';
+const AWS = require('aws-sdk');
+const ssm = new AWS.SSM({ region: 'us-west-2' });
+
+const SPIRA_URL = "SPIRA_URL_ROOT=https://science37.spiraservice.net/services/v5_0/RestService.svc"
+const SPIRA_USER = "Kiran"
+const SPIRA_KEY = "/spira/kiran"
 
 exports.handler = async (event, context, callback) => {
   const options = {
@@ -8,6 +14,10 @@ exports.handler = async (event, context, callback) => {
     payload: event.body,
     headers: event.headers,
   };
+
+  const request = await ssm.getParameter({ Name: SPIRA_KEY }).promise();
+  const SPIRA_PASSWORD = request.Parameter.Value.toString();
+
   const response = {
     statusCode: 200,
     headers: {
@@ -15,6 +25,7 @@ exports.handler = async (event, context, callback) => {
     },
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
+      password: SPIRA_PASSWORD,
       input: event,
     }),
   };
