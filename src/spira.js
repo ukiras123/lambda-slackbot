940dac31ad15d1f75c7074a0053e94eafd7aca9d
“kiran37” <kiran.gautam@science37.com>
2019-03-15 17:20:16 -0700

    adding spira paramter store

--- src/spira.js
+++ src/spira.js
@@ -7,14 +7,15 @@ const ssm = new AWS.SSM({
 });
 
 const SPIRA_URL = "https://science37.spiraservice.net/services/v5_0/RestService.svc"
-const SPIRA_USER = "Kiran"
-const SPIRA_KEY = "/spira/kiran"
+const SPIRA_USER = "/spira/name";
+const SPIRA_PASS = "/spira/password";
 
 class Spira {
     constructor() {
         this.apiKey = null;
+        this.name = null;
     }
-    
+
     async createConnection(){
         const headers = await this.getHeader();
         const connection = axios.create({
@@ -24,26 +25,35 @@ class Spira {
         });
         return connection;
     }
-    
+
     async getHeader() {
         return {
-            Accept: 'application/json',
+            'Accept': 'application/json',
             'Content-Type': 'application/json',
-            username: SPIRA_USER,
+            'username': await this.getSpiraName(),
             'api-key': await this.getSpiraKey(),
         };
     }
 
+    async getSpiraName(){
+        if(!this.name){
+            const request = await ssm.getParameter({Name: SPIRA_USER}).promise();
+            this.name = request.Parameter.Value.toString();
+        }
+        return this.name;
+    }
+
     async getSpiraKey(){
         if(!this.apiKey){
-            const request = await ssm.getParameter({Name: SPIRA_KEY}).promise();
+            const request = await ssm.getParameter({Name: SPIRA_PASS}).promise();
             this.apiKey = request.Parameter.Value.toString();
         }
         return this.apiKey;
     }
 
+
     async getReleases() {
-        try {            
+        try {
             const connection = await this.createConnection();
             const response = await connection.get('/projects/9/releases');
             return response.data;
